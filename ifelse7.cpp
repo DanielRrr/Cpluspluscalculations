f91828442e7db9fe724e098253b6f50d15b0717c
DanielRrr <chelsea2005joecole@icloud.com>
2016-03-31 16:11:48 +0300

    Create ifelse7.cpp

---
+++ ifelse7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+    if (c == a + 1 && d == b || c == a - 1 && d == b) {cout << "YES";}
+    else if (c == a && d == b + 1 || c == a && d == b - 1) {cout << "YES";}
+    else if (c == a + 1 && d == b - 1 || c == a - 1 && d == b + 1) {cout << "YES";}
+    else if (c == a + 1 && d == b + 1 || c == a - 1 && d == b - 1) {cout << "YES";}
+    else {cout << "NO";}
+  return 0;
+}
