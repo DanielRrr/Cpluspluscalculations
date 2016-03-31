23aabb4e4ea91178b75be016cf40d8eb30d8a793
DanielRrr <chelsea2005joecole@icloud.com>
2016-03-31 16:13:53 +0300

    Create ifelse10.cpp

---
+++ ifelse10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+    if (c == a - 1 && d == b - 2) {cout << "YES";}
+    else if (c == a + 1 && d == b - 2) {cout << "YES";}
+    else if (c == a - 2 && d == b - 1) {cout << "YES";}
+    else if (c == a + 2 && d == b - 1) {cout << "YES";}
+    else if (c == a - 1 && d == b + 2) {cout << "YES";}
+    else if (c == a + 1 && d == b + 2) {cout << "YES";}
+    else if (c == a - 2 && d == b + 1) {cout << "YES";}
+    else if (c == a + 2 && d == b + 1) {cout << "YES";}
+    else {cout << "NO";}
+  return 0;
+}
