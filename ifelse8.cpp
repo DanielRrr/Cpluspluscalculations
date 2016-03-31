6cba56925f8d404c1b40fcf4edf83be16f1ff731
DanielRrr <chelsea2005joecole@icloud.com>
2016-03-31 16:12:49 +0300

    Create ifelse8.cpp

---
+++ ifelse8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+    if (abs (a - c) == abs (b - d)) {cout << "YES";}
+    else {cout << "NO";}
+  return 0;
+}
