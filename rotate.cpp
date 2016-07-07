49add5aec78a5b07699c211d6e6923046676c461
Danile Rogozin <chelsea2005joecole@icloud.com>
2016-07-07 12:59:41 +0300

    Create rotate.cpp

---
+++ rotate.cpp
@@ -0,0 +1,12 @@
+void rotate(int a[], unsigned size, int shift)
+{
+    if (shift == 0) return;
+    for (int k=0;k<shift;k++){
+        int temp = a[0];
+        for (int i=0;i<(size-1);i++)
+            a[i] = a[i+1];
+        a[size-1] = temp;
+    }
+    
+    
+}
